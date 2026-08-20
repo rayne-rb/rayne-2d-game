4e1fac7e78e049c0fff4bb86f34b876d4fd104c7
Rayne Binneman <rayne.r.binneman@gmail.com>
2026-08-20 10:35:40 +0200

    Implement player movement mechanics, add camera and input actions, and update platform scaling

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,25 +1,79 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
-	[Export]
-	public float Speed = 12f;
-	[Export] 
-	public float Acceleration = 10f;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [ExportGroup("Movement")] [Export] public float Speed = 45f;
+    [Export] public float Acceleration = 16f;
+    [Export] public float Deceleration = 18f;
+    [Export] public float JumpStrength = 20f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (Input.IsActionPressed("move_right"))
-		{
-			GD.Print("Right");
-			this.Position += new Vector2(Speed * (float)delta, this.Position.Y);
-		}
-	}
+    [ExportGroup("Physics")] [Export] public float Gravity = -20f;
+
+    [ExportGroup("General")] [Export] public Sprite2D PlayerSprite;
+
+    public bool IsGrounded = false;
+    public bool IsJumping = false;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("Player Ready");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        IsGrounded = IsOnFloor();
+
+        if (IsGrounded)
+        {
+            if (Input.IsActionPressed("jump"))
+            {
+                velocity.Y = -JumpStrength;
+                IsJumping = true;
+            }
+            else
+            {
+                IsJumping = false;
+            }
+        }
+        else
+        {
+            if (IsJumping)
+            {
+                if (velocity.Y < 0.0 && !Input.IsActionPressed("jump"))
+                {
+                    // velocity.Y -=  0.5f * Gravity * JumpStrength * (float)delta;
+                    velocity.Y = 0;
+                }
+                else
+                {
+                    velocity.Y -= Gravity * (float)delta;
+                }
+            }
+            else
+            {
+                velocity.Y -= Gravity * (float)delta;
+            }
+        }
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X = Mathf.Lerp(velocity.X, Speed, Acceleration * (float)delta);
+        }
+        else if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X = Mathf.Lerp(velocity.X, -Speed, Acceleration * (float)delta);
+        }
+        else
+        {
+            velocity.X = Mathf.Lerp(velocity.X, 0, Deceleration * (float)delta);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 }
